e3b5776130554b0c23de5ccb03bd16549b593f5b
Rodrigo Azevedo <42504436+rodrigoazevedo1988@users.noreply.github.com>
2025-12-17 20:21:10 -0300

    commit inicial do projeto

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const converterde = document.querySelector("#converter-de");
+const converterpara = document.querySelector("#converter-para");
+
+console.log(converterde.innerHTML, converterpara.innerHTML);
+
+/* if (converterde.value = converterpara.value){
+    alert("Valores iguais")
+}; */
